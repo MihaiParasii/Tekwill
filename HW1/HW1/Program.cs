c27805ed9dd912ce3d710d61f3f3002063ff6200
MihaiParasii <160401024+MihaiParasii@users.noreply.github.com>
2024-11-22 13:54:57 +0200

    Add files via upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HW1
+{
+    public class MyClasa
+    {
+        private int _var1;
+        public string Nume;
+
+        public static void AfiseazaNumar(int numar)
+        {
+            if (numar > 0)
+            {
+                Console.WriteLine("Numarul este pozitiv.");
+            }
+            else
+            {
+                Console.WriteLine("Numarul este negativ.");
+            }
+        }
+
+        public static void CalculeazaSuma(int a, int b)
+        {
+            int suma = a + b;
+            Console.WriteLine("Suma numerelor este: " + suma);
+        }
+
+        public static void CalculeazaProdus(int a, int b)
+        {
+            int produs = a * b;
+            Console.WriteLine("Produsul numerelor este: " + produs);
+        }
+    }
+
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            MyClasa obj = new MyClasa();
+
+            MyClasa.AfiseazaNumar(10);
+
+            obj.Nume = "Exemplu";
+            Console.WriteLine(obj.Nume);
+
+            MyClasa.CalculeazaSuma(5, 3);
+            MyClasa.CalculeazaProdus(2, 4);
+
+            Console.ReadLine();
+        }
+    }
+}
